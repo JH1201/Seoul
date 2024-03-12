f730b47ba87c1fc5af43c1e7f82f3b86acd67f2d
yoooohee <146921017+yoooohee@users.noreply.github.com>
2024-03-12 13:51:44 +0900

    HomeController안에 mapPageController 추가

--- src/main/java/com/project/Seoul/controller/HomeController.java
+++ src/main/java/com/project/Seoul/controller/HomeController.java
@@ -2,29 +2,36 @@ package com.project.Seoul.controller;
 
 import com.project.Seoul.domain.CultureInfo;
 import com.project.Seoul.service.HomeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @Controller
 public class HomeController {
 
+    @Autowired
     public final HomeService homeService;
 
     public HomeController(HomeService homeService) {
         this.homeService = homeService;
     }
 
-    @GetMapping("/home")
+    @GetMapping({"/home"})
     public String homepage(Model model) {
-        List<CultureInfo> list = homeService.getAllCultureInfoApiSortedByMonth();
-        model.addAttribute("list", list);
+        List<CultureInfo> list = homeService.getAllCultureInfoApi();
+
+        model.addAttribute("lists", list);
         return "/homepage/home";
     }
 
-
-
+    // HomeController.java 또는 적절한 컨트롤러 파일 내에 추가
+    @GetMapping("/mapPage")
+    public String allEvents(Model model) {
+        List<CultureInfo> allEvents = homeService.getAllCultureInfoApi(); // 모든 문화행사를 가져오는 서비스 메서드
+        model.addAttribute("searchResults", allEvents);
+        return "homepage/mapPage"; // 모든 문화행사를 포함한 mapPage를 반환
+    }
 }
