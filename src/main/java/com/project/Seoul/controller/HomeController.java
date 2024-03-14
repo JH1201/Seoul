7e5e7803735925f7bdf9465cc9fc04820d6fbdba
yoooohee <146921017+yoooohee@users.noreply.github.com>
2024-03-14 13:42:02 +0900

    Add files via upload

--- src/main/java/com/project/Seoul/controller/HomeController.java
+++ src/main/java/com/project/Seoul/controller/HomeController.java
@@ -1,7 +1,6 @@
 package com.project.Seoul.controller;
 
 import com.project.Seoul.domain.CultureInfo;
-import com.project.Seoul.domain.FavoriteCultureInfo;
 import com.project.Seoul.service.HomeService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -11,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -34,33 +32,14 @@ public class HomeController {
             homeService.saveEvents(cultureInfo);
 
         }
-
         List<CultureInfo> items = homeService.getAllCultureInfoApi(); // API로부터 아이템들을 가져오는 메서드
         Collections.shuffle(items); // 아이템들을 랜덤하게 섞는다
         List<CultureInfo> selectedItems = items.subList(0, 5); // 상위 5개 아이템을 선택
         model.addAttribute("items", selectedItems); // 모델에 아이템들을 추가
         model.addAttribute("lists", list);
-
-        /*
-        List<FavoriteCultureInfo> allFavoriteEvents = homeService.getAllFavoriteEvents();
-
-        // list 내의 각 `CultureInfo` 객체에 대해 `allFavoriteEvents`와 비교하여 isFavorite 설정
-        for (CultureInfo cultureInfo : list) {
-            for (FavoriteCultureInfo favEvent : allFavoriteEvents) {
-                if (cultureInfo.getTITLE().equals(favEvent.getTITLE())) { // 또는 적절한 비교 로직
-                    cultureInfo.setFavorite(true);
-                    break;
-                }
-            }
-        }
-
-         */
-
-
         return "/homepage/home";
     }
 
-    /*
     @PostMapping("/home")
     public ResponseEntity<?> saveFavoriteEvent(@RequestBody Map<String, Long> payload) {
         Long cultureInfoId = payload.get("cultureInfoId");
@@ -69,27 +48,14 @@ public class HomeController {
         // ID를 사용하여 cultureInfo를 찾고, 이벤트를 저장하는 로직을 수행합니다.
         homeService.findAndSaveEventById(cultureInfoId);
 
-
-        // 반환할 데이터를 포함하는 맵 생성
-        Map<String, Long> response = new HashMap<>();
-        response.put("cultureInfoId", cultureInfoId);
-        System.out.println("cultureInfoId = " + cultureInfoId);
-
-        // ResponseEntity에 담아 JSON 형태로 반환
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok().build();
     }
 
-     */
-
     @GetMapping("/monthlySort")
-    public String sort(@RequestParam("month") String month, Model model) {
-
+    @ResponseBody
+    public ResponseEntity<List<CultureInfo>> sort(@RequestParam("month") String month) {
         List<CultureInfo> list = homeService.getDropBoxData(month);
-
-        model.addAttribute("lists", list);
-        model.addAttribute("selectedMonth", month); // 선택된 월을 모델에 추가
-
-        return "/homepage/home";
+        return ResponseEntity.ok(list); // JSON 형식으로 데이터 반환
     }
 
 }
